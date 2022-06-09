d787738b017304357b83160144552b1bce937f81
Emanuel López <emanuel.lopez@globant.com>
2022-06-09 15:55:53 -0300

    Adds http client mock test

--- flutter-api-testing/test/fetch_book_test.dart
+++ flutter-api-testing/test/fetch_book_test.dart
@@ -1,5 +1,43 @@
+import 'package:apitesting/fetch_books.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
 
+import 'fetch_book_test.mocks.dart';
+
+@GenerateMocks([http.Client])
 void main() {
-  group('Fetch book API call test', () {});
+  group('Fetch book API call test', () {
+    test('Should return a list of books for http success call', () async {
+      // Arrange
+      final mockClient = MockClient();
+
+      when(mockClient.get(Uri.parse(fetchBooksURL))).thenAnswer(
+        (realInvocation) async => http.Response(
+          '[{ "name": "The 5 Second Rule", "auther": "Mel Robbins"}]',
+          200,
+        ),
+      );
+
+      // Act & Assert
+      expect(await fetchBooks(mockClient), isA<List<BooksListModel>>());
+    });
+
+    test('Should throw and exception when http call finished with an error',
+        () {
+      // Arrange
+      final mockClient = MockClient();
+
+      when(mockClient.get(Uri.parse(fetchBooksURL))).thenAnswer(
+        (realInvocation) async => http.Response(
+          'Not found',
+          404,
+        ),
+      );
+
+      // Act & Assert
+      expect(fetchBooks(mockClient), throwsException);
+    });
+  });
 }
